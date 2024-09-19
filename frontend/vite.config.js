5288e80cd5b67108871d5e95249d774b55503625
Sooraj Sanker <soorajshankar@gmail.com>
2024-09-19 19:50:06 +0530

    style changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@console': path.resolve(__dirname, './packages/console'),
-            '@': path.resolve(__dirname, './packages'),
-        },
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  resolve: {
+    alias: {
+      '@console': path.resolve(__dirname, './packages/console'),
+      '@': path.resolve(__dirname, './packages'),
     },
+  },
 });
