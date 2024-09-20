3a647c1b34385bd57d656259ae2b3948a56b09a6
Phil Freeman <phil@hasura.io>
2024-09-20 12:04:23 -0700

    websocket protocol

--- pacha/data_engine/python_executor.py
+++ pacha/data_engine/python_executor.py
@@ -32,18 +32,54 @@ class PythonExecutor:
     output_text: str = ""
     error: Optional[str] = None
     modified_artifact_identifiers: list[str] = field(default_factory=list)
-   
+    
+    def maybe_cancel(self):
+        current_task = asyncio.current_task()
+        if current_task is not None and current_task.cancelled():
+            self.output_text += "User Cancellation Requested...\n"
+            raise asyncio.CancelledError('Cancelled Python Program')
+        
+    def output(self, text: str):
+        self.maybe_cancel()
+        self.output_text += str(text) + '\n'
+        
     async def exec_code(self, code: str):
         try:
+            from websockets.asyncio.client import connect
+            from json import dumps, loads
+            
             self.hooks.on_python_execute(code)
             
-            headers = {"Content-type": "application/json"}
-            data = {"python": code, "config": {
-                "engine_url": "http://localhost:3000",
-                "anthropic_api_key": os.environ.get('ANTHROPIC_API_KEY')
-            }}
-            response = requests.post("http://localhost:3001/execute", json=data, headers=headers)
-            self.output_text = response.text
+            async with connect("ws://localhost:3001/") as websocket:
+                data = {
+                    "python": code, 
+                    "config": {
+                        "engine_url": "http://localhost:3000",
+                        "anthropic_api_key": os.environ.get('ANTHROPIC_API_KEY')
+                    }
+                }
+                
+                await websocket.send(dumps(data))
+                
+                async for message_json in websocket:
+                    message = loads(message_json)
+                    
+                    match message['type']:
+                        case "print": 
+                            self.output(message['text'])
+                        case "store_artifact":
+                            contents = loads(message['contents'])
+                            output = self.context.artifacts.store_artifact(
+                                message['identifier'], contents['title'], contents['artifact_type'], contents['data'])
+                            self.modified_artifact_identifiers.append(message['identifier'])
+                            self.output(output)
+                        case "get_artifact":
+                            artifact = copy.deepcopy(self.context.artifacts.get_artifact(message['identifier']))
+                            await websocket.send(dumps({
+                                "orig_msg_id": message['msg_id'],
+                                "contents": artifact
+                            }))
+
             
             self.hooks.on_python_output(self.output_text)
         except Exception as e:
