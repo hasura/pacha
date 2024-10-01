0babb2ed86f91c5ac72679cbc936e707439fe689
Abhinav Gupta <127770473+abhinav-hasura@users.noreply.github.com>
2024-10-01 10:50:37 -0700

    move websocket client to scripts

--- new_code/playground/scripts/websocket_test_client.py
+++ new_code/playground/scripts/websocket_test_client.py
@@ -7,11 +7,15 @@ from promptql_playground import protocol
 
 
 async def websocket_client():
+    # This is for continuing an existing thread.
     # uri = "ws://localhost:5000/threads/865d85e4-c78c-4e14-9487-5ea5cb40d836/continue"
+
+    # Ths is for starting a new thread
     uri = "ws://localhost:5000/threads/start"
     async with websockets.connect(uri) as websocket:
         # Send a message
-        message = {"type": "client_init", "version": "v1"}
+        message = {"type": "client_init", "version": "v1",
+                   "ddn_headers": {"x-hasura-role": "admin"}}
         await websocket.send(json.dumps(message))
         message = {"type": "user_message",
                    "message": "issue $10 in credits to the latest project"}
@@ -22,16 +26,18 @@ async def websocket_client():
             while True:
                 response = await websocket.recv()
                 print(f"Received: {response}")
-                try:
-                    server_message = RootModel[protocol.ServerMessage].model_validate_json(
-                        response).root
-                    if isinstance(server_message, protocol.UserConfirmationRequest):
-                        await websocket.send(json.dumps({"type": "user_confirmation_response", "response": "approve", "confirmation_request_id": str(server_message.confirmation_request_id)}))
-                except:
-                    pass
+                server_message = RootModel[protocol.ServerMessage].model_validate_json(
+                    response).root
+                if isinstance(server_message, protocol.UserConfirmationRequest):
+                    await websocket.send(json.dumps({"type": "user_confirmation_response", "response": "approve", "confirmation_request_id": str(server_message.confirmation_request_id)}))
         except websockets.exceptions.ConnectionClosedOK:
             pass
 
 
-# Run the client
-asyncio.run(websocket_client())
+def main():
+    # Run the client
+    asyncio.run(websocket_client())
+
+
+if __name__ == "__main__":
+    main()
