eddcf41d96f8cf6b2ec147c869b4efda221804ec
Sooraj Sanker <soorajshankar@gmail.com>
2024-10-16 09:15:26 +0000

    Sync app/features/chat to frontend/src/chat

--- frontend/src/chat/data/hooks.ts
+++ frontend/src/chat/data/hooks.ts
@@ -1,29 +1,38 @@
-import { useContext, useMemo } from 'react';
+import { useMemo } from 'react';
+import { useProjectContext } from '@console/context/hooks';
 
 import { createQuery } from '@/utils/react-query';
-import { PachaChatContext } from '../PachaChatContext';
+import { usePachaChatContext } from '../PachaChatContext';
 import { ChatClient } from './client';
 
-export const getLocalChatClient = (baseUrl: string, authToken: string) =>
+export const getLocalChatClient = (baseUrl: string, authToken?: string) =>
   new ChatClient({
     baseUrl,
     authToken,
   });
 
 export const usePachaLocalChatClient = () => {
-  const { pachaEndpoint, authToken } = useContext(PachaChatContext);
+  const { pachaEndpoint, authToken } = usePachaChatContext();
+  const { buildVersion, projectId } = useProjectContext();
+
   return useMemo(() => {
     return new ChatClient({
       baseUrl: pachaEndpoint,
       authToken: authToken,
+      projectId,
+      buildVersion,
     });
-  }, [pachaEndpoint, authToken]);
+  }, [pachaEndpoint, authToken, projectId, buildVersion]);
 };
 
-export const useThreads = (baseURL: string, authToken: string) => {
+export const useThreads = (
+  baseURL: string,
+  authToken?: string,
+  projectId?: string
+) => {
   return createQuery({
     queryKey: ['chat-threads', baseURL, authToken],
-    fetcher: () => getLocalChatClient(baseURL, authToken).getThreads(),
+    fetcher: () => getLocalChatClient(baseURL, authToken).getThreads(projectId),
   })({
     select: data => {
       const reversed = [...data];
@@ -35,7 +44,7 @@ export const useThreads = (baseURL: string, authToken: string) => {
 
 export const usePachaConnectionStatus = (
   baseURL: string,
-  authToken: string
+  authToken?: string
 ) => {
   return createQuery({
     queryKey: ['pacha-connection-health', baseURL, authToken],
