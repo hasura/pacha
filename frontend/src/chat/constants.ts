1292a3c8a4cee1b6ff7b8f790e4b9ff974b1e01c
Sooraj Sanker <soorajshankar@gmail.com>
2024-10-17 06:00:33 +0000

    Sync app/features/chat to frontend/src/chat

--- frontend/src/chat/constants.ts
+++ frontend/src/chat/constants.ts
@@ -1,8 +1,8 @@
 import { getRoutes } from '@/routing';
 
-export const DEFAULT_PACHA_ENDPOINT =
+export const DEFAULT_PROMPTQL_PLAYGROUND_ENDPOINT =
   getRoutes().localDev.defaultPachaEndpoint(); // default will be fed externally from the app (console/ pacha repo)
 
-export const PACHA_CHAT_CONFIG_LOCAL_STORAGE_KEY = 'pacha-chat-config';
+export const PROMPTQL_PLAYGROUND_CONFIG_LOCAL_STORAGE_KEY = 'pacha-chat-config';
 
 export const AUTH_TOKEN_HEADER_KEY = 'pacha_auth_token';
