3be247af25d848d8954a7eaba7ba42b1ba153b12
Sooraj Sanker <soorajshankar@gmail.com>
2024-10-17 18:09:44 +0000

    Sync app/features/chat to frontend/src/chat

--- frontend/src/chat/PachaChatContext.tsx
+++ frontend/src/chat/PachaChatContext.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { Dispatch, RefObject, SetStateAction, useContext } from 'react';
 
-import { DEFAULT_PACHA_ENDPOINT } from './constants';
+import { Thread } from './data/api-types';
+import { Artifact, NewAiResponse } from './types';
 
-export const PachaChatContext = React.createContext<{
-  isMinimized: boolean;
-  setIsMinimized: (b: boolean) => void;
-  pachaEndpoint: string;
-  setPachaEndpoint: (endpoint: string) => void;
-  authToken: string;
-  setAuthToken: (token: string) => void;
-}>({
-  isMinimized: false,
-  setIsMinimized: b => {},
-  pachaEndpoint: DEFAULT_PACHA_ENDPOINT,
-  setPachaEndpoint: endpoint => {},
-  authToken: '',
-  setAuthToken: token => {},
-});
+export const PachaChatContext = React.createContext<
+  | {
+      isMinimized: boolean;
+      setIsMinimized: (b: boolean) => void;
+      pachaEndpoint: string;
+      setPachaEndpoint: (endpoint: string) => void;
+      authToken?: string;
+      setAuthToken: (token?: string) => void;
+      data: NewAiResponse[];
+      setRawData: Dispatch<SetStateAction<NewAiResponse[]>>;
+      artifacts: Artifact[];
+      setArtifacts: Dispatch<SetStateAction<Artifact[]>>;
+      threads: Thread[];
+      isThreadsLoading: boolean;
+      refetchThreads: () => void;
+      threadsError: Error | null;
+      mode: 'cloud' | 'local';
+      routes: {
+        newChat: string;
+        chatThreadLink: (threadId: string) => string;
+      };
+      setHeadersRef: (h: Record<string, string>) => void;
+      headersRef: RefObject<Record<string, string>>;
+    }
+  | undefined
+>(undefined);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const usePachaChatContext = () => {
+  const context = useContext(PachaChatContext);
+  if (!context) {
+    throw Error('PachaChatContext is not provided');
+  }
+
+  return context;
+};
