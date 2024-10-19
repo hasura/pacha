843ce7fdebb7b42f0951baeed5c786b342082c52
Sooraj Sanker <soorajshankar@gmail.com>
2024-10-19 19:32:47 +0000

    Sync packages/ui to frontend/packages/ui

--- frontend/packages/ui/hooks/index.ts
+++ frontend/packages/ui/hooks/index.ts
@@ -10,5 +10,6 @@ export * from './useSchemeColors';
 export * from './useMRTDefaultOptions';
 export * from './useHoverableTextStyle';
 export * from './useUIState';
-
 export { useLocalStorage, useSessionStorage } from './useStorage';
+export type { StorageHookProps } from './useStorage';
+export * from './useHasOverflowX';
