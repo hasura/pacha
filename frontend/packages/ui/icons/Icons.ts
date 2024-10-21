821e06be1f8a9b50764541df909e947502d1de0c
Sooraj Sanker <soorajshankar@gmail.com>
2024-10-21 13:59:51 +0000

    Sync packages/ui to frontend/packages/ui

--- frontend/packages/ui/icons/Icons.ts
+++ frontend/packages/ui/icons/Icons.ts
@@ -1,5 +1,6 @@
 import { MRT_Icons } from 'mantine-react-table';
 import { BiGitCompare, BiLogoPostgresql, BiRefresh } from 'react-icons/bi';
+import { BsSortDown, BsSortUp } from 'react-icons/bs';
 import { CgReadme } from 'react-icons/cg';
 import { CiCloudOff, CiCloudOn } from 'react-icons/ci';
 import {
@@ -16,14 +17,14 @@ import {
   FaRegBuilding,
   FaTerminal,
 } from 'react-icons/fa';
-import { FiTable } from 'react-icons/fi';
+import { FiMenu, FiTable } from 'react-icons/fi';
 import { HiOutlineArrowRight, HiOutlineSupport } from 'react-icons/hi';
 import { HiOutlineArchiveBox } from 'react-icons/hi2';
 import { IoMdCheckmarkCircleOutline } from 'react-icons/io';
 import { IoEaselOutline } from 'react-icons/io5';
 import { LiaUserCheckSolid, LiaUserClockSolid } from 'react-icons/lia';
 import { LuBuilding2, LuServer } from 'react-icons/lu';
-import { MdOutlinePublic } from 'react-icons/md';
+import { MdHttp, MdOutlinePublic } from 'react-icons/md';
 import {
   PiAlignCenterVertical,
   PiArrowClockwise,
@@ -54,6 +55,7 @@ import {
   PiCaretUp,
   PiCaretUpDown,
   PiChartBar,
+  PiChartBarFill,
   PiChartDonut,
   PiChartLine,
   PiChatCircle,
@@ -63,10 +65,12 @@ import {
   PiCheckCircleFill,
   PiChecks,
   PiCircle,
+  PiCircleFill,
   PiClock,
   PiClockCounterClockwise,
   PiCloud,
   PiCloudLightning,
+  PiCode,
   PiCodeBlock,
   PiCodeSimple,
   PiCompass,
@@ -79,6 +83,7 @@ import {
   PiDatabase,
   PiDatabaseFill,
   PiDoor,
+  PiDotsThreeDuotone,
   PiDotsThreeOutline,
   PiDotsThreeOutlineVertical,
   PiDotsThreeVertical,
@@ -107,6 +112,7 @@ import {
   PiLockSimple,
   PiMagnifyingGlass,
   PiMagnifyingGlassMinus,
+  PiMapPinArea,
   PiMapTrifold,
   PiMapTrifoldFill,
   PiMonitor,
@@ -138,9 +144,8 @@ import {
   PiSkipForward,
   PiSliders,
   PiSlidersHorizontal,
-  PiSortAscending,
-  PiSortDescending,
   PiStack,
+  PiStarLight,
   PiStorefront,
   PiSun,
   PiTable,
@@ -152,12 +157,12 @@ import {
   PiTrash,
   PiTrendDown,
   PiTrendUp,
-  PiUser,
   PiUserCircle,
   PiUsers,
   PiWallet,
   PiWarning,
   PiWarningCircleFill,
+  PiWrench,
   PiX,
   PiXCircle,
 } from 'react-icons/pi';
@@ -190,10 +195,16 @@ export const MetadataIcons = {
 };
 
 export const ProjectRoles = {
-  ProjectOwner: PiShieldCheck,
-  ProjectAdmin: PiKey,
-  ProjectUser: PiUser,
+  ProjectOwner: PiStarLight,
+  ProjectAdmin: PiShieldCheck,
+  ProjectUser: PiEye,
   ProjectExecuteGraphQL: PiTerminal,
+  SubgraphDev: PiCode,
+  SubgraphAdmin: PiWrench,
+};
+
+export const ProjectAccess = {
+  AccessLevel: PiChartBarFill,
 };
 
 // Basic App Actions
@@ -229,6 +240,7 @@ export const NavbarIcons = {
   SettingsActive: PiGearFill,
   Insights: PiEye,
   Changes: PiGitBranch,
+  TakeTour: PiMapPinArea,
 };
 
 // Visualization
@@ -378,6 +390,10 @@ export const MiscIcons = {
   TrendUp: PiTrendUp,
   TrendDown: PiTrendDown,
   DocsLink: TbNotes,
+  Menu: FiMenu,
+  Eye: PiEye,
+  FillCircle: PiCircleFill,
+  Headers: MdHttp,
 };
 
 export const ShareProjectIcons = {
@@ -412,7 +428,7 @@ export const MantineReactTableIcons: Partial<MRT_Icons> = {
   IconClearAll: PiX,
   IconColumns: PiTextColumns,
   IconDeviceFloppy: PiFloppyDisk,
-  IconDots: PiDotsThreeOutline,
+  IconDots: PiDotsThreeDuotone,
   IconDotsVertical: PiDotsThreeVertical,
   IconEdit: PiPencil,
   IconEyeOff: PiEyeSlash,
@@ -426,13 +442,13 @@ export const MantineReactTableIcons: Partial<MRT_Icons> = {
   IconPinnedOff: PiPushPinSlash,
   IconSearch: PiMagnifyingGlass,
   IconSearchOff: PiMagnifyingGlassMinus,
-  IconSortAscending: PiSortAscending,
-  IconSortDescending: PiSortDescending,
+  IconSortAscending: BsSortDown,
+  IconSortDescending: BsSortUp,
   IconX: PiX,
 };
 
 export const DDNPlanIcons = {
-  DDNFree: PiUser,
+  DDNFree: PiUsers,
   DDNBase: FaRegBuilding,
   DDNAdvanced: LuBuilding2,
   DDNPrivate: LuServer,
@@ -465,4 +481,5 @@ export const Icons = {
   ...ShareProjectIcons,
   ...DDNInfraIcons,
   ...ChatIcons,
+  ...ProjectAccess,
 };
